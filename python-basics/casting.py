dd0ee2b184aad74aab03f68bced55f6048c1acb4
Erniklol <167703611+Erniklol@users.noreply.github.com>
2026-01-25 01:32:12 +0500

    Add casting examples

---
+++ python-basics/casting.py
@@ -0,0 +1,26 @@
+#CASTING
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+
+a = str("s1") # x will be 's1'
+b = str(2)    # y will be '2'
+
+c = float(1)     # x will be 1.0
+d = float(2.8)   # y will be 2.8
+
+print(x,y)
+print(a,b)
+print(c,d)
+
+age_str = "17"      
+age = int(age_str)    
+
+print(age)
+print(type(age))
+
+price = "19.99"      
+quantity = 3             
+
+total = float(price) * quantity
+print(total)
+print(type(total))
